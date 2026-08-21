b206f7393721acec14c246cf1ab34bdd8cc39d4f
Insener-leiutaja <kevin.pajula@hariduse.edu.ee>
2026-08-21 13:59:24 +0200

    Update app.js

--- snapmaker-feed/app.js
+++ snapmaker-feed/app.js
@@ -193,7 +193,7 @@
       v.controls = true;
       v.loop = true;
       v.muted = false;
-      v.autoplay = falce;
+      v.autoplay = false;
       v.playsInline = true;
       v.setAttribute("playsinline", "playsinline");
 
